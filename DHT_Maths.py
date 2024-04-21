a6eeb96136ebe8280b92df6903e7ebf82c89a039
tdevaharsha <167551732+tdevaharsha@users.noreply.github.com>
2024-04-21 22:32:40 +0530

    Add files via upload

---
+++ DHT_Maths.py
@@ -0,0 +1,38 @@
+Mathematics class 11
+
+Chapter 1: Sets
+
+Important Points:
+- A set is a well-defined collection of objects.
+- A set which does not contain any element is called the            empty set or the null set or the void set.
+-A set which is empty or consists of a definite number of elements is called finite otherwise, the set is called infinite.
+-Two sets A and B are said to be equal if they have exactly the same elements and we write A = B. Otherwise, the sets are said to be unequal and we write A ≠ B.
+-A set A is said to be a subset of a set B if every element of A is also an element of B. We denote it as A ⊂ B.
+-The union of A and B is the set which consists of all the elements of A and all the elements of B. The symbol ‘∪’ is used to denote the union.
+Some Properties of the Operation of Union
+(i) A ∪ B = B ∪ A (Commutative law)
+(ii) (A ∪ B) ∪ C = A ∪ (B ∪ C) (Associative law)
+(iii) A ∪ φ = A (Law of identity element, φ is the identity of ∪)
+(iv) A ∪ A = A (Idempotent law)
+(v) U ∪ A = U (Law of U)
+-The intersection of sets A and B is the set of all elements which are common to both A and B. The symbol ‘∩’ is used to denote the intersection.
+-Some Properties of Operation of Intersection
+(i) A ∩ B = B ∩ A (Commutative law).
+(ii) (A ∩ B) ∩ C = A ∩ (B ∩ C) (Associative law).
+(iii) Φ ∩ A = φ, U ∩ A = A (Law of φ and U).
+(iv) A ∩ A = A (Idempotent law)
+(v) A ∩ (B ∪ C) = (A ∩ B) ∪ (A ∩ C) (Distributive law)
+-The difference of the sets A and B in this order is the set
+of elements which belong to A but not to B. Symbolically, we write A – B and read as
+“A minus B”
+-Let U be the universal set and A a subset of U. Then the complement of A is the set of all elements of U which are not the elements of A.
+-Some Properties of Complement Sets
+ 1. Complement laws: (i) A ∪ A′ = U (ii) A ∩ A′ = φ
+2. De Morgan’s law: (i) (A ∪ B) ´ = A′ ∩ B′ (ii) (A ∩ B) ′ = A′ ∪ B′
+3. Law of double complementation: (A′) ′ = A
+4. Laws of empty set and universal set φ′ = U and U′ = φ
+
+
+
+
+
